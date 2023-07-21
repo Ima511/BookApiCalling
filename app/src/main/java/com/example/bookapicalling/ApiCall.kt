3f598b0ad20231692fd068a8981fac672334854b
Ima511 <abhaykumarjhaexam420@rediffmail.com>
2023-07-21 15:51:32 +0530

    this is the 2nd version of the api calling.

--- app/src/main/java/com/example/bookapicalling/ApiCall.kt
+++ app/src/main/java/com/example/bookapicalling/ApiCall.kt
@@ -9,9 +9,26 @@ import io.ktor.util.InternalAPI
 @OptIn(InternalAPI::class)
 suspend fun main() {
     val client = HttpClient(CIO)
-    val response: HttpResponse = client.get("https://books.googleapis.com/books/v1/volumes?q=android")
-    println("Response Body :- " + response.bodyAsText())
-    println("Response Content :- " + response.content)
-    println(response.status)
+
+    // Get the keyword from the user
+    println("Enter the keyword to search for books:")
+    val keyword = readLine()
+
+    if (keyword != null && keyword.isNotBlank()) {
+        // Construct the API URL with the user-provided keyword
+        val apiUrl = "https://books.googleapis.com/books/v1/volumes?q=${keyword.trim()}"
+
+        try {
+            val response: HttpResponse = client.get(apiUrl)
+            println("Response Body :- " + response.bodyAsText())
+            println("Response Content :- " + response.content)
+            println(response.status)
+        } catch (e: Exception) {
+            println("Error occurred: ${e.message}")
+        }
+    } else {
+        println("Invalid keyword. Please enter a valid keyword.")
+    }
+
     client.close()
 }
